21ffb0e00ebefb11b92486815369177544295355
Ayana Bando <ayanaflax@yahoo.com>
2018-10-15 16:28:23 +0000

    automatically backed up by learn

--- db/migrate/01_create_user.rb
+++ db/migrate/01_create_user.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.decimal :balance
       t.string :password_digest
+      t.decimal :balance
     end
   end
 end
